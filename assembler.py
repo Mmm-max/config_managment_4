c4d2781beed11cfeb05d3b0c56b954ac204c7e56
Mmm-max <em.solopov@gmail.com>
2024-11-20 21:37:36 +0300

    fix reading of binary file

--- assembler.py
+++ assembler.py
@@ -6,14 +6,14 @@ COMANDS = {
     'LOAD_CONST': 20,
     'READ_MEM': 140,
     'WRITE_MEM': 232,
-    'BIN_OP_AND': 83
+    'BIN_OP_AND': 219
 }
 
 COMMANDS_SIZE = {
-    20: 6,
-    140: 5,
-    232: 5,
-    83: 8
+    20: 7,
+    140: 6,
+    232: 6,
+    219: 9
 }
 
 class Coder:
@@ -23,7 +23,8 @@ class Coder:
     def read(self, file_name):
         with open(file_name, 'r') as f:
             for line in f.readlines():
-                opcode, *operands = line.strip().split(' ')
+                opcode, *operands = line.strip().split(', ')
+                print(f'first operands: {operands}')
                 self.instructions.append(Instruction(opcode, operands))
     
     def write(self, file_name):
@@ -32,9 +33,9 @@ class Coder:
                 f.write(instruction.encode())
             
 class Instruction:
-    def __init__(self, opcode, *operands):
+    def __init__(self, opcode, operands):
         self.opcode = opcode
-        self.operands = operands
+        self.operands = list(map(int, operands))
     
     def __str__(self):
         return f'{self.opcode} {", ".join(self.operands)}'
@@ -46,9 +47,15 @@ class Instruction:
         if self.opcode not in COMANDS:
             raise ValueError(f'Unknown command {self.opcode}')
         self.opcode = COMANDS[self.opcode]
+        size = COMMANDS_SIZE[self.opcode] * 8
         if self.opcode == 20:    # const
             const, adress = self.operands
-            packed_data = (self.opcode << 48) | (const << 24) | (adress << 6)
+            print(f'const {const}, adress {adress}')
+            print(f'const {const} ({bin(const)}), adress {adress} ({bin(adress)})')
+            print(f'shift opcode: {size - 7}, shift const: {size - 26}, shift adress: {size - 18}')
+            packed_data = (self.opcode << (size - 8)) | (const << (size - 34)) | (adress << (size - 52))
+            for i in packed_data.to_bytes(7, byteorder='big'):
+                print(f'i: {i} - {i:08b}')
             bit_arr.frombytes(packed_data.to_bytes(7, byteorder='big'))
         elif self.opcode == 140:   # read
             read_adress, write_adress = self.operands
@@ -84,8 +91,50 @@ class Assembler:
                 opcode = int.from_bytes(opcode, byteorder='big')
                 if opcode not in COMANDS.values():
                     raise ValueError(f'Unknown command {opcode}')
+                bit_arr = bitarray()
+                # size in bytes
+                size = (COMMANDS_SIZE[opcode] - 1) # opcode already read
                 match opcode:
                     case 20: # const
-                        size = COMMANDS_SIZE[opcode]
+                        print("const")
                         data = f.read(size)
-                        const, adress = struct.unpack('>ii', data)
+                        # const 8th - 33th bit, adress 34th - 51th bit
+                        bit_arr.frombytes(data)
+                        # const = int(bit_arr[8:34].to01(), 2)
+                        # adress = int(bit_arr[34:52].to01(), 2)
+                        const = bit_arr[0:34 - 8].to01()
+                        adress = bit_arr[34 - 8:52 - 8].to01()
+                        print(f'bit_arr {bit_arr.to01()}')
+                        print(f'const {int(const, 2)} - {const}, adress {int(adress, 2)} - {adress}')
+                    case 140:
+                        data = f.read(size)
+                        # read_adress 8th - 25th bit, write_adress 26th - 43th bit
+                        bitarray.frombytes(data)
+                        read_adress = int(bit_arr[0:26 - 8].to01(), 2)
+                        write_adress = int(bit_arr[26 - 8:44 - 8].to01(), 2)
+                    case 232: # write
+                        data = f.read(size)
+                        # read_adress 8th - 25th bit, write_adress 26th - 43th bit
+                        bitarray.frombytes(data)
+                        read_adress = int(bit_arr[0:26 - 8].to01(), 2)
+                        write_adress = int(bit_arr[26 - 8:44 - 8].to01(), 2)
+                    case 219: # bin_op_and
+                        data = f.read(size)
+                        # b_adress 8th - 25th bit, bias 26th - 40th bit, adress_d 41th - 63th bit, adress_e 64th - 88th bit
+                        bitarray.frombytes(data)
+                        b_adress = int(bit_arr[0:26 - 8].to01(), 2)
+                        bias = int(bit_arr[26 - 8:31 - 8].to01(), 2)
+                        adress_d = int(bit_arr[31 - 8:49 - 8].to01(), 2)
+                        adress_e = int(bit_arr[49 - 8:67 - 8].to01(), 2)
+                    case _:
+                        raise ValueError(f'Unknown command {opcode}')
+
+
+
+def main():
+    coder = Coder()
+    coder.read('input.txt')
+    coder.write('output.bin')
+    
+    assembler = Assembler()
+    assembler.read_bytecode('output.bin')
